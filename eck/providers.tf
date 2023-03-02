6a75aff8d9028ac10ae642677cf4a0b606e13087
Max Hu <maxxh@google.com>
2023-03-03 02:04:34 +0800

    first commit

--- eck/providers.tf
+++ eck/providers.tf
@@ -51,6 +51,7 @@ provider "kubectl" {
   client_certificate     = base64decode(data.google_container_cluster.cluster.master_auth.0.client_certificate)
   client_key             = base64decode(data.google_container_cluster.cluster.master_auth.0.client_key)
   cluster_ca_certificate = base64decode(data.google_container_cluster.cluster.master_auth.0.cluster_ca_certificate)
+  load_config_file = "false"
 }
 
 # provider "kubernetes" {
