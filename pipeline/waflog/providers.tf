b00dfba00552ae023d20aa87cf36d50112327942
Max Hu <maxxh@google.com>
2023-03-06 12:58:55 +0800

    first commit

--- pipeline/waflog/providers.tf
+++ pipeline/waflog/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
     bucket = "BACKEND_BUCKET_NAME"
-    prefix = "terraform/state/pipeline/dnslog"
+    prefix = "terraform/state/pipeline/waflog"
   }
   required_providers {
     google = {
