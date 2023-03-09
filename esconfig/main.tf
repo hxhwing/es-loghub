b0183649afc2521acc0b75d3b0f13b7dff5b826c
Max Hu <maxxh@google.com>
2023-03-09 21:31:49 +0800

    first commit

--- esconfig/main.tf
+++ esconfig/main.tf
@@ -26,7 +26,7 @@ resource "elasticstack_elasticsearch_index_template" "auditlog_template" {
 
   # priority       = 42
   index_patterns = ["AUDITLOG_INDEX_PREFIX*"]
-  data_stream {}
+  # data_stream {}
 
   template {
     # alias {
@@ -35,7 +35,7 @@ resource "elasticstack_elasticsearch_index_template" "auditlog_template" {
 
     settings = jsonencode({
       # "index.routing.allocation.include._tier_preference": ["data_hot"],
-      # "index.routing.allocation.include.data": "hot",
+      "index.routing.allocation.include.data": "hot",
       "number_of_replicas":1
     })
   }
@@ -46,7 +46,7 @@ resource "elasticstack_elasticsearch_index_template" "dnslog_template" {
 
   # priority       = 42
   index_patterns = ["DNSLOG_INDEX_PREFIX*"]
-  data_stream {}
+  # data_stream {}
 
   template {
     # alias {
@@ -55,7 +55,7 @@ resource "elasticstack_elasticsearch_index_template" "dnslog_template" {
 
     settings = jsonencode({
       # "index.routing.allocation.include._tier_preference": ["data_hot"],
-      # "index.routing.allocation.include.data": "hot",
+      "index.routing.allocation.include.data": "hot",
       "number_of_replicas":1
     })
   }
@@ -66,7 +66,7 @@ resource "elasticstack_elasticsearch_index_template" "httplog_template" {
 
   # priority       = 42
   index_patterns = ["HTTPLOG_INDEX_PREFIX*"]
-  data_stream {}
+  # data_stream {}
 
   template {
     # alias {
@@ -75,7 +75,7 @@ resource "elasticstack_elasticsearch_index_template" "httplog_template" {
 
     settings = jsonencode({
       # "index.routing.allocation.include._tier_preference": ["data_hot"],
-      # "index.routing.allocation.include.data": "hot",
+      "index.routing.allocation.include.data": "hot",
       "number_of_replicas":1
     })
     mappings = jsonencode({
