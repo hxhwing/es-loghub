2ba3aa4b51e523c52aaee2d60a70848c368002f6
Xinhua Hu <maxxh@google.com>
2023-03-13 10:30:38 +0800

    first commit

--- esconfig/main.tf
+++ esconfig/main.tf
@@ -67,7 +67,7 @@ resource "elasticstack_elasticsearch_index_template" "dnslog_template" {
     # }
 
     settings = jsonencode({
-      "index.routing.allocation.include._tier_preference" : ["data_warm"],
+      "index.routing.allocation.include._tier_preference" : "data_warm",
       # "index.routing.allocation.include.data" : "hot",
       "number_of_replicas" : 1
     })
@@ -87,7 +87,7 @@ resource "elasticstack_elasticsearch_index_template" "httplog_template" {
     # }
 
     settings = jsonencode({
-      # "index.routing.allocation.include._tier_preference": ["data_hot"],
+      # "index.routing.allocation.include._tier_preference": "data_hot",
       "index.routing.allocation.include.data" : "hot",
       "number_of_replicas" : 1
     })
